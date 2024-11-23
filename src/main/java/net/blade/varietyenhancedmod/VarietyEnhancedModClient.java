0c608e1849b94b651a17aae59405ee235f33d977
GGBladeGG <willtreg2004@gmail.com>
2024-11-23 22:00:52 +0000

    Initial commit

---
+++ src/main/java/net/blade/varietyenhancedmod/VarietyEnhancedModClient.java
@@ -0,0 +1,10 @@
+package net.blade.varietyenhancedmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VarietyEnhancedModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
